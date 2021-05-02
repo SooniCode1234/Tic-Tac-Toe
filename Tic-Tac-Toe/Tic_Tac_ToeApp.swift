52a61723cefafc47e34d5f6c7dd5664fb5742e6f
Sooni-Code1234 <sooni.mohammed@gmail.com>
2021-05-02 19:12:29 -0400

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Sooni Mohammed on 2021-05-02.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
