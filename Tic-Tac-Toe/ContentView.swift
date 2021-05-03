a22b420528f890cb3887e3703f16282b85f0f1b8
Sooni-Code1234 <sooni.mohammed@gmail.com>
2021-05-02 20:30:24 -0400

    Started the win condition

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -8,9 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isGameboardDisabled = false
+    
     var body: some View {
-        Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red).opacity(0.5)
+                                .frame(width: geometry.size.width / 3 - 15,
+                                       height: geometry.size.width / 3 - 15)
+                            
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            moves[i] = Move(player: .human, boardIndex: i)
+                            isGameboardDisabled = true
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosistion(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isGameboardDisabled = false
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .disabled(isGameboardDisabled)
             .padding()
+        }
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosistion(in moves: [Move?]) -> Int {
+        var movePosistion = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosistion) {
+            movePosistion = Int.random(in: 0..<9)
+        }
+        
+        return movePosistion
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves.compactMap { $0}.filter { $0.player == player }
+        let playerPositions = Set((playerMoves.map { $0.boardIndex }))
+        
+        
+        return true
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
